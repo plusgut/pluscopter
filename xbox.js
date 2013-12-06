962f89f066217bda9b4e88c7aa605d8c71353012
Carlo Jeske <github@webentwickler2-0.de>
2013-12-06 18:36:20 +0100

    added some beauty

--- xbox.js
+++ xbox.js
@@ -6,84 +6,76 @@ var mapping  = require('./mapping.json');
 var stick        = new Joystick(0, 3500, 350);
 var droneClient  = arDrone.createClient();
 
-function handleButton(evt) {
-	handleEvent(evt, 'buttons');
-}
-
-function handleStick(evt) {
-	handleEvent(evt, 'axis');
-}
-
-function handleEvent(evt, type) {
-	var scope = mapping[type];
-	var found = false;
-	for(var i = 0; i <scope.length; i++) {
-		var single = scope[i];
-		if(evt.number === single.number) {
-			found = true;
-
-			if(type == 'axis') {
-				executeMove(evt, single);
-			} else if(joy[single.action]) {
-				
-				if(evt.value === 1) {
-					joy[single.action](evt, single.payload);
-				}
-			} else{
-				console.log(single.action + ' did not exist');
-			}
-		}
-	}
-
-	if(!found) {
-		console.log(evt.number);
-	}
-}
-
-function executeMove(evt, map) {
-	var func = null;
-	if(evt.value >= 0) {
-		func = map.positive;
-	} else if(evt.value < 0){
-		func = map.negative;
-	}
-
-	speed = joy.grade(evt.value);
-	execute(func, speed);
-}
-
-function execute(func, payload, extra) {
-	console.log(func, payload);
-	try{
-		droneClient[func](payload, extra);
-	} catch(err){
-		console.log(err);
-	}
-}
-
-stick.on('button', handleButton);
-stick.on('axis', handleStick);
-
 var joy = {
 	started: false,
 	duration: 1000,
+	execute: function(action, payload, extra) {
+		console.log(action, payload);
+		try{
+			droneClient[action](payload, extra);
+		} catch(err){
+			console.log(err);
+		}
+	},
+	executeMove: function(evt, map) {
+		var action = null;
+		if(evt.value >= 0) {
+			action = map.positive;
+		} else if(evt.value < 0){
+			action = map.negative;
+		}
+
+		speed = joy.grade(evt.value);
+		this.execute(action, speed);
+	},
+	handleButton: function(evt) {
+		joy.handleEvent(evt, 'buttons');
+	},
+	handleStick: function(evt) {
+		joy.handleEvent(evt, 'axis');
+	},
+	handleEvent: function(evt, type) {
+		var scope = mapping[type];
+		var found = false;
+		for(var i = 0; i <scope.length; i++) {
+			var single = scope[i];
+			if(evt.number === single.number) {
+				found = true;
+
+				if(type == 'axis') {
+					this.executeMove(evt, single);
+				} else if(this[single.action]) {
+					
+					if(evt.value === 1) {
+						this[single.action](evt, single.payload);
+					}
+				} else{
+					console.log(single.action + ' did not exist');
+				}
+			}
+		}
+
+		if(!found) {
+			console.log(evt.number);
+		}
+	},
 	toggleStart: function() {
 		if(this.started) {
-			execute('land');
+			this.execute('land');
 		} else {
-			execute('takeoff');
+			this.execute('takeoff');
 		}
 
 		this.started = !this.started;
 	},
 	exec: function(evt, type) {
-		execute(type);
+		this.execute(type);
 	},
 	animate: function(evt, type) {
-		execute('animate', type, this.duration);
+		this.execute('animate', type, this.duration);
 	},
 	calibrate: function() {
-		execute('calibrate', 0);
+		this.execute('calibrate', 0);
 	},
 	grade: function(value){
 		if(value < 0){
@@ -93,3 +85,7 @@ var joy = {
 		return value / 35000;
 	}
 };
+
+
+stick.on('button', joy.handleButton);
+stick.on('axis', joy.handleStick);
